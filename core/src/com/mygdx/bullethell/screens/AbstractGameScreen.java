8bf4ed5b21bfba7e58a96b5156697cbde13626c1
Aaron Wink <winkpso2.3@gmail.com>
2018-12-13 07:15:15 -0500

    Ported the entire menu system from Canyon Bunny. Now to clean it up.

---
+++ core/src/com/mygdx/bullethell/screens/AbstractGameScreen.java
@@ -0,0 +1,40 @@
+
+package com.mygdx.bullethell.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
+import com.mygdx.bullethell.game.Assets;
+
+/**
+ * Abstract class; basis for GameScreen and MenuScreen
+ * @author Aaron Wink
+ */
+public abstract class AbstractGameScreen implements Screen
+{
+    protected Game game;
+    
+    public AbstractGameScreen(Game game)
+    {
+        this.game = game;
+    }
+    
+    /**
+     * Abstract methods
+     */
+    public abstract void render(float deltaTime);
+    public abstract void resize(int width, int height);
+    public abstract void show();
+    public abstract void hide();
+    public abstract void pause();
+    
+    public void resume()
+    {
+        Assets.instance.init(new AssetManager());
+    }
+    
+    public void dispose()
+    {
+        Assets.instance.dispose();
+    }
+}
